77032ddee5b0d6782a10d0122ffa40ba817bb95c
Michael <huff600@hotmail.com>
2016-06-18 23:10:54 -0600

    DATABASE and some of Josh's code

--- app/src/main/java/com/example/huff6/clientbox/MainActivity.java
+++ app/src/main/java/com/example/huff6/clientbox/MainActivity.java
@@ -1,28 +1,45 @@
 package com.example.huff6.clientbox;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.provider.CallLog;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
     //We need a thread singleton object here
+    DatabaseHelper myDb;
 
     public static final String MESSAGE = "com.example.huff6.clientbox.AddClientActivity";
     public static final String MESSAGE2 = "com.example.huff6.clientbox.TimerActivity";
     public static final String MESSAGE3 = "com.example.huff6.clientbox.ManualEntryActivity";
     public static final String MESSAGE4 = "com.example.huff6.clientbox.ClientLookupActivity";
+
+    TextView call;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //eye h8 meyecall <3
+        // T~T < so mean )
         //View temp = findViewById(R.id.awesome);
 
 
         Intent intent = getIntent();
         String message = intent.getStringExtra(LoginActivity.EXTRA_MESSAGE);
+
+        call = (TextView) findViewById(R.id.call);
+        updateCallInfo();
     }
 
     public void goToAddClient(View v){
@@ -77,5 +94,46 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    void updateCallInfo(){
+        StringBuilder sb = new StringBuilder();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG) != PackageManager.PERMISSION_GRANTED) {
+            System.out.println("No permissions");
+            return;
+        }
+        Cursor managedCursor = getContentResolver().query(CallLog.Calls.CONTENT_URI,
+                null, null, null, null);
 
+        assert managedCursor != null;
+        int number = managedCursor.getColumnIndex(CallLog.Calls.NUMBER);
+        int type = managedCursor.getColumnIndex(CallLog.Calls.TYPE);
+        int date = managedCursor.getColumnIndex(CallLog.Calls.DATE);
+        int duration = managedCursor.getColumnIndex(CallLog.Calls.DURATION);
+
+        while (managedCursor.moveToNext()) {
+            String phNumber = managedCursor.getString(number);
+            String callType = managedCursor.getString(type);
+            String callDate = managedCursor.getString(date);
+            Date callDayTime = new Date(Long.valueOf(callDate));
+            String callDuration = managedCursor.getString(duration);
+            String dir = null;
+            int dircode = Integer.parseInt(callType);
+            switch (dircode) {
+                case CallLog.Calls.OUTGOING_TYPE:
+                    dir = "OUTGOING";
+                    break;
+
+                case CallLog.Calls.INCOMING_TYPE:
+                    dir = "INCOMING";
+                    break;
+
+                case CallLog.Calls.MISSED_TYPE:
+                    dir = "MISSED";
+                    break;
+            }
+            // ADD TO TABLE
+            myDb.insertData("UNKNOWN", phNumber, callDayTime.toString(),
+                    "SOME TIME", callDuration, dir);
+        }
+        managedCursor.close();
+    }
 }
